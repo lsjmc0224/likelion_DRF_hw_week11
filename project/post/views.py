79a5c8bfd60d7e000841101b4d1a82c2f582af48
Sangjun Lee <lsjmc0224@dgu.edu>
2023-07-16 18:18:27 +0900

    POST 기능 추가

--- project/post/views.py
+++ project/post/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
+from .models import Post
+from .serializers import PostSerializer
 
 # Create your views here.
+class PostViewSet(viewsets.GenericViewSet, 
+                  mixins.ListModelMixin, 
+                  mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin, 
+                  mixins.DestroyModelMixin):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
