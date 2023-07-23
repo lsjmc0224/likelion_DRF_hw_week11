bc2afa3aa978ed9e5689c8245da18c4ace76cb66
lsjmc0224 <lsjmc0224@dgu.edu>
2023-07-23 21:13:55 +0900

    changed postslistview & commentqueryset

--- project/post/serializers.py
+++ project/post/serializers.py
@@ -9,12 +9,47 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = '__all__'
 
+#list view (list 메소드로 불렀을 때)
+class PostListSerializer(serializers.ModelSerializer):
+    #content 말고 content개수만 보이기
+    comments_cnt = serializers.SerializerMethodField()
+    
+    def get_comments_cnt(self, instance):
+        return instance.comments.count()
+    
+    #comments 개수만 보이게 하고싶음.
+    class Meta:
+        model = Post
+        fields = [
+            'id',
+            'created_at',
+            'updated_at',
+            'title',
+            'writer',
+            'content',
+            'comments_cnt',
+        ]
+        read_only_fields = ['id', 'created_at', 'updated_at', 'comments_cnt']
 
+#detail view (list 이외의 메소드로 불렀을 때)
 class PostSerializer(serializers.ModelSerializer):
-    comments = serializers.SerializerMethodField(read_only=True)
+    # read_only = True가 아닌 필드와 serializer메소드필드 유지
+    comments = serializers.SerializerMethodField()
+    title = serializers.CharField()
+    writer = serializers.CharField()
+    content = serializers.CharField()
+
     def get_comments(self, instance):
         serializer = CommentSerializer(instance.comments, many=True)
         return serializer.data
+    
+    #여기선 comments를 보이게 하고 싶음.
     class Meta:
         model = Post
         fields = '__all__'
+        read_only_fields = [
+            'id',
+            'created_at',
+            'updated_at',
+            'comments',
+        ]
--- project/post/views.py
+++ project/post/views.py
@@ -3,36 +3,45 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
+from .serializers import PostSerializer, CommentSerializer, PostListSerializer
 
 # Create your views here.
-class PostViewSet(viewsets.GenericViewSet, 
-                  mixins.ListModelMixin, 
-                  mixins.CreateModelMixin,
-                  mixins.RetrieveModelMixin,
-                  mixins.UpdateModelMixin, 
-                  mixins.DestroyModelMixin):
+class PostViewSet(
+    viewsets.GenericViewSet, 
+    mixins.ListModelMixin, 
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin, 
+    mixins.DestroyModelMixin
+    ):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return PostListSerializer
+        return PostSerializer
 
-class CommentViewSet(viewsets.GenericViewSet,
-                     mixins.RetrieveModelMixin,
-                     mixins.UpdateModelMixin, 
-                     mixins.DestroyModelMixin):
+class CommentViewSet(
+    viewsets.GenericViewSet,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin, 
+    mixins.DestroyModelMixin
+    ):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
-class PostCommentViewSet(viewsets.GenericViewSet,
-                          mixins.ListModelMixin,
-                          mixins.CreateModelMixin):
-    queryset = Comment.objects.all()
+class PostCommentViewSet(
+    viewsets.GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin
+    ):
     serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
 
-    def list(self, request, post_id=None):
-        post = get_object_or_404(Post, id=post_id)
-        queryset = self.filter_queryset(self.get_queryset().filter(post=post))
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+    #url에서 추출된 post아이디를 가져오는 것
+    def get_queryset(self):
+        post = self.kwargs.get("post_id")
+        queryset = Comment.objects.filter(post_id=post)
+        return queryset
     
     def create(self, request, post_id=None):
         post = get_object_or_404(Post, id=post_id)
